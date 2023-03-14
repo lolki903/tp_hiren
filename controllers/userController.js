c53d894534709a51409ff4aea4ffbcd4dad3b748
Jules <jeanfreza428@gmail.com>
2023-03-14 14:13:00 +0100

    register

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,27 +1,31 @@
 let jwt = require('jsonwebtoken');
 let bcrypt = require('bcryptjs');
 let userModels = require('../models/userModel');
+let my = require('../config/database');
 
 // Register a new user
 exports.register = function (req, res) {
     try{
         let hashedPassword = bcrypt.hashSync(req.body.password, 8);
-        userModels.create({
-            name : req.body.name,
-            email : req.body.email,
-            password : hashedPassword
-        }, function (err, user) {
-            if (err) return res.status(500).send("There was a problem registering the user.")
-            // create a token
-            let token = jwt.sign({ id: user.id }, process.env.SECRET, {
-                expiresIn: '2023-12-12' // expires in 24 hours
-            });
-            res.status(200).send({ auth: true, token: token });
+        let sql = "INSERT INTO users (firstname, lastname, status) VALUES (?,?,?)";
+        my.query(sql, [req.body.fisrtname, req.body.lastname, req.body.status], (err, results) => {
+            if (err) throw err;
+            res.send(results);
         });
     }catch(err){
         res.send(err);
     }
     }
 
-// Login a user
-     
+// update a user
+exports.getUser = function (req, res) {
+    try{
+        let sql = "SELECT * FROM users WHERE id = ?";
+        my.query(sql, [req.params.id], (err, results) => {
+            if (err) throw err;
+            res.send(results);
+        });
+    }catch(err){
+        res.send(err);
+    }
+}
