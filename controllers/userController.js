84d54afb9df96f3a01e6dedb07fdb1eb2f93716e
Jules <jeanfreza428@gmail.com>
2023-03-14 15:21:52 +0100

    restructuration des methodes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,29 +1,36 @@
 let jwt = require('jsonwebtoken');
 let bcrypt = require('bcryptjs');
 let userModels = require('../models/userModel');
-let my = require('../config/database');
+
 
 // Register a new user
 exports.register = function (req, res) {
     try{
-        let hashedPassword = bcrypt.hashSync(req.body.password, 8);
-        let sql = "INSERT INTO users (firstname, lastname, status) VALUES (?,?,?)";
-        my.query(sql, [req.body.fisrtname, req.body.lastname, req.body.status], (err, results) => {
-            if (err) throw err;
-            res.send(results);
+       userModels.create(req.body.firstname, req.body.lastname, req.body.status, (err, result) => {
+            res.send(result);
         });
     }catch(err){
         res.send(err);
     }
     }
 
-// update a user
+// get one user
 exports.getUser = function (req, res) {
     try{
-        let sql = "SELECT * FROM users WHERE id = ?";
-        my.query(sql, [req.params.id], (err, results) => {
+       userModels.find(req.params.id, (err, result) => {
+            res.send(result);
+        });
+    }catch(err){
+        res.send(err);
+    }
+}
+
+// get all users
+exports.getUsers = function (req, res) {
+    try{
+       userModels.all((err, result) => {
             if (err) throw err;
-            res.send(results);
+            res.send(result);
         });
     }catch(err){
         res.send(err);
