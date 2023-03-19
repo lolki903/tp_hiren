340507e76079026a0880e9b128071aec99148f5c
julesgaborit <jules.gaborit@my-digital-school.org>
2023-03-19 19:56:47 +0100

    ajout routes

--- routes/restauranttableRoute.js
+++ routes/restauranttableRoute.js
@@ -1,7 +1,10 @@
 module.exports = (app) => {
   const restauranttablecontroller = require("../controllers/restauranttableController");
-  app
-    .route("/restauranttable")
+  app.route("/restauranttable")
     .post(restauranttablecontroller.create)
     .get(restauranttablecontroller.list);
+  app.
+    route("/restauranttable/:id")
+    .put(restauranttablecontroller.update)
+    // .delete(restauranttablecontroller.delete);
 };
--- routes/statRoute.js
+++ routes/statRoute.js
@@ -4,4 +4,6 @@ module.exports = (app) => {
     app.route("/stat").get(stat.total);
     app.route("/statuser/:id").get(stat.tipsuser);
     app.route("/statmonth/:id").get(stat.monthtips);
+    // app.route("/statweek/:id").get(stat.weektips);
+
     }
